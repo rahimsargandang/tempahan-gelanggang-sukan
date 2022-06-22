1811f708ab6aaa91326a956c39fd679c366bb198
rahimsargandang <a178804@siswa.ukm.edu.my>
2022-06-22 10:58:55 +0800

    Register and login UI

--- app/src/main/java/com/example/aplikasitempahangelanggangsukan/login.java
+++ app/src/main/java/com/example/aplikasitempahangelanggangsukan/login.java
@@ -2,13 +2,28 @@ package com.example.aplikasitempahangelanggangsukan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
+
+    private Button Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Login = findViewById(R.id.btn_login);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this, homepage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
